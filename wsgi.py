dd0ca0d4f06f62b0fcc1b43bd754950ca311cf60
TecnoRV <dacegarcia19@gmail.com>
2020-02-18 12:21:35 -0600

    Update output message Main .py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World this is my first OpenShift Demo"
 
 if __name__ == "__main__":
     application.run()
